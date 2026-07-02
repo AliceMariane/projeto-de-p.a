e9e364cdd02e39caac6aa08207a378f02bb2ab66
Raí.Santos <70606883+F3ryG4rd@users.noreply.github.com>
2026-07-02 20:06:15 -0300

    Update circulo.py

--- AppPaint/Figuras/circulo.py
+++ AppPaint/Figuras/circulo.py
@@ -1,16 +1,44 @@
 from AppPaint.Figuras.figura import Figura
 
 class Circulo(Figura):
+  '''
+  Representa um círculo desenhado pelo usuário.
 
+  O círculo é definido por:
+    > centro (cx, cy)
+    > raio
+
+  O centro é definido no primeiro clique do mouse.
+
+  Enquanto o usuário arrasta o mouse,
+  o raio é recalculadoem tempo real utilizando
+  a distância entre o centro e a posição atual do mouse.
+  '''
+  
   def __init__(self, x, y, cor_borda= "black", cor_preenchimento= ''):
     super().__init__(cor_borda, cor_preenchimento)
+    '''
+    Cria um novo círculo.
+
+    O primeiro clique define o centro do círculo.
+
+    Inicialmente o raio vale zero,
+    sendo atualizado durante o movimento do mouse.
+    '''
+    
+    # Coordenadas do Centro
     self.cx = x
     self.cy = y
+    
+    # Raio Inicial
     self.raio = 0
     
   def atualizar(self, x, y):
     '''
-    Atualiza o tamanho do raio
+    Atualiza o raio do Círculo
+    
+    O raio é a distância do centro
+    até o ponto atual do mouse
     '''
     
     self.raio = ((self.cx)**2 +
@@ -45,7 +73,10 @@ class Circulo(Figura):
   
   def incompleta(self):
     '''
-    Só vira círculo se o raio for maior q 0
+    Verifica se o círculo pode ser criado.
+    
+    O círculo é considerado inválido
+    se o raio for igual a 0
     '''
     
     return self.raio == 0
