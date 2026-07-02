7a4809bca6220fd8e12a907eef04e2039057c15b
Raí.Santos <70606883+F3ryG4rd@users.noreply.github.com>
2026-07-02 20:07:57 -0300

    Update figura.py

--- AppPaint/Figuras/figura.py
+++ AppPaint/Figuras/figura.py
@@ -1,5 +1,5 @@
 class Figura:
-    """
+    '''
     Classe base para todas as figuras.
 
     Toda figura tem:
@@ -8,15 +8,16 @@ class Figura:
         
     Toda figura pode:
         > ser desenhada
+        > ser vista enquanto desenhada 
         > ser atualizada enquanto o mouse está pressionado
         > verificar se está incompleta
 
     Cada subclasse irá implementar sua própria forma de desenhar e atualizar.
-    """
+    '''
 
     def __init__(self, cor_borda= "black", cor_preenchimento= ""):
-        """
-        Atributos de classe:
+        '''
+        Atributos de Classe:
 
         Parâmetros
         ----------
@@ -25,33 +26,34 @@ class Figura:
 
         cor_preenchimento : str
             Cor do Preenchimento.
-        """
+        
 
         self.cor_borda = cor_borda
         self.cor_preenchimento = cor_preenchimento
+        '''
 
     def atualizar(self, x, y):
-        """
+        '''
         Atualiza o ultimo ponto (x, y) da figura
         Será implementado pelas subclasses
-        """
+        '''
         pass
 
     def desenhar(self, canvas):
-        """
+        '''
         Desenha a figura
         Será implementado pelas subclasses
-        """
+        '''
         pass
 
     def desenhar_preview(self, canvas):
-        """
+        '''
         Preview da figura atual
-        """
+        '''
         pass
 
     def incompleta(self):
-        """
+        '''
         Verifica se a figura pode ser salva
-        """
+        '''
         return False
