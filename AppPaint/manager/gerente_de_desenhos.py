8a36a81b9e2e9f7aba91f0cc3ae0a5c3c2e9cf42
Alice Mariane <ari60781326@gmail.com>
2026-07-02 23:04:34 -0300

    Improve comments and variable descriptions
    Refactor comments and improve variable descriptions in GerenteDeDesenhos class.

--- AppPaint/manager/gerente_de_desenhos.py
+++ AppPaint/manager/gerente_de_desenhos.py
@@ -1,18 +1,17 @@
-#a fabrica tá na mesma pasta de gerente de desenhos
 from fabrica_de_figuras import FabricaDeFiguras
 
 class GerenteDeDesenhos:
     '''
     classe que controla e define os desenhos do paint
 '''
-    def __init__(self, canvas, gerente_de_cores): #percebi que parte do que tem aqui tinha no main da primeira etapa
+    def __init__(self, canvas, gerente_de_cores):
         self.figuras= []#armazenar figuras
-        self.canvas=canvas #os desenhos serao feitos aqui
+        self.canvas=canvas #os desenhos serao feitos no canvas
         self.desenho_atual= None #default
-        self.desenho= 'Reta'#padrao
-        self.gerente_de_cores= gerente_de_cores
+        self.desenho= 'Reta'#figura padrao
+        self.gerente_de_cores= gerente_de_cores #cores para as figuras
         
-    def iniciar_figura(self, event): #usa a fabrica pra montar o desenhos com os ingredientes 
+    def iniciar_figura(self, event): #comeca uma figura
         self.desenho_atual= FabricaDeFiguras.criar(
             self.desenho,
             event.x,
@@ -33,13 +32,13 @@ class GerenteDeDesenhos:
         self.desenhar_fig()
     
     def desenhar_fig (self):
-        #desenha as figuras que estao prontas
-        self.canvas.delete('all')#os desenhos estao guardados mas a tela apaga
+        #desenha na tela as figuras que estao prontas e guardadas
+        self.canvas.delete('all')#os desenhos estao guardados, o que apaga pra atualizar eh o que aparece na tela
         
         for desenho in self.figuras:#redesenha na tela os desenhos que estao guardados
             desenho.desenhar(self.canvas)
         
-        #figuras novas(se desenho atual nao for nada)
+        #quando se desenha figuras novas(se desenho atual nao for nada)
         #if self.desenho_atual
         if self.desenho_atual is not None:
             self.desenho_atual.desenhar(self.canvas)
