d696008069d54c40257fce56810148d571ab3193
Alice Mariane <ari60781326@gmail.com>
2026-07-02 22:57:30 -0300

    Enhance comments in gerente_de_cores.py
    Updated comments for clarity on color selection methods.

--- AppPaint/manager/gerente_de_cores.py
+++ AppPaint/manager/gerente_de_cores.py
@@ -3,22 +3,19 @@ class GerenteDeCores:
     Define as cores das figuras
     '''
     def __init__ (self):
-        self.cor_borda= 'black' #cor padrao, pra que seria rosa?
-        self.cor_preenchimento=''
+        self.cor_borda= 'black' #cor padrao
+        self.cor_preenchimento='' #preenchimento inicial/padrao(vazio)
         
         
-    def color_select_outline(self, nova_cor): #cor que o usuario escolheu
+    def color_select_outline(self, nova_cor): #cor que o usuario escolheu para a borda/linha das figuras
         self.cor_borda= nova_cor
 
-    def color_select_fill(self, nova_cor): #cor que o usuario escolheu
+    def color_select_fill(self, nova_cor): #cor que o usuario escolheu para o preenchimento das giguras
         self.cor_preenchimento= nova_cor
     
-    def cor_atual_da_linha (self):#a cor que vai usar nas figuras
+    def cor_atual_da_linha (self):#cor utilizada atualmente na borda/linha das figuras
         return self.cor_borda
 
-    def cor_atual_do_preenchimento(self):
+    def cor_atual_do_preenchimento(self):#cor utilizada atualmente no preenchimento das figuras
         return self.cor_preenchimento
 
-
-#talvez eu volte aqui
-        
