72c353753106b2f61964690f349b8016ff41f1e7
Alice Mariane <ari60781326@gmail.com>
2026-07-02 22:16:35 -0300

    Add method to create figure objects in FabricaDeFiguras
    Implemented a static method to create figure objects based on user choice.

--- AppPaint/manager/fabrica_de_figuras.py
+++ AppPaint/manager/fabrica_de_figuras.py
@@ -1,6 +1,38 @@
+from Figuras.reta import Reta
+from Figuras.circulo import Circulo
+from Figuras.elipse import Elipse
+from Figuras.mao_livre import MaoLivre
+from Figuras.retangulo import Retangulo
+from Figuras.quadrado import Quadrado
+
+
 class FabricaDeFiguras:
     '''
-    --.- ..- . .-. .. .- / . ... - .- .-. / ..-. .- --.. . -. -.. --- /..- -- / -.. . ... . -. .... --- / . -. --. .-. .- -.-. .- -.. ---
+    cria os objetos das classes de figuras de acordo com a escolha do usuario
     '''
-    def __init__ (self):
-        pass
+    
+    @staticmethod
+    def criar (desenho,x, y, cor_borda, cor_preenchimento):
+
+        # estrutura----> if desenho == 'n', return a classe N com o que 'n' usa
+        if desenho=='Reta':
+            return Reta(x,y,cor_borda)
+        
+        elif desenho=='Retangulo':
+            return Retangulo(x,y,cor_borda,cor_preenchimento)
+        
+        elif desenho=='Circulo':
+            return Circulo(x,y,cor_borda, cor_preenchimento)
+        
+        elif desenho=='Elipse':
+            return Elipse(x,y,cor_borda, cor_preenchimento)
+        
+        elif desenho=='Quadrado':
+            return Quadrado(x,y,cor_borda, cor_preenchimento)
+        
+        else:#desenha a mao livre==rabisco
+            return MaoLivre(x,y,cor_borda)
+        
+        
+#aparentemente a classe Figura eh um asset para estruturar classes de figuras futuras
+#poxa essa classe aqui parece a mais facil :/
