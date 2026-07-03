af38398c59a26caffe1fab7f5c41adf67900b2c0
Alice Mariane <ari60781326@gmail.com>
2026-07-03 20:41:51 -0300

    Refactor attributes to use private naming convention

--- AppPaint/manager/gerente_de_desenhos.py
+++ AppPaint/manager/gerente_de_desenhos.py
@@ -5,47 +5,48 @@ class GerenteDeDesenhos:
     classe que controla e define os desenhos do paint
 '''
     def __init__(self, canvas, gerente_de_cores):
-        self.figuras= []#armazenar figuras
-        self.canvas=canvas #os desenhos serao feitos no canvas
-        self.desenho_atual= None #default
-        self.desenho= 'Reta'#figura padrao
-        self.gerente_de_cores= gerente_de_cores #cores para as figuras
+        self._figuras= []#armazenar figuras
+        self._canvas=canvas #os desenhos serao feitos no canvas
+        self._desenho_atual= None #default
+        self._desenho= 'Reta'#figura padrao
+        self._gerente_de_cores= gerente_de_cores #cores para as figuras
         
     def iniciar_figura(self, event): #comeca uma figura
-        self.desenho_atual= FabricaDeFiguras.criar(
-            self.desenho,
+        self._desenho_atual= FabricaDeFiguras.criar(
+            self._desenho,
             event.x,
             event.y,
-            self.gerente_de_cores.cor_borda,
-            self.gerente_de_cores.cor_preenchimento
+            self._gerente_de_cores.cor_borda,
+            self._gerente_de_cores.cor_preenchimento
             
         )
         
     def update_fig(self,event):#atualiza a  figura que o usuario esta fazendo e redesenha (desenhar_fig)
-        if self.desenho_atual:
-          self.desenho_atual.update( event.x, event.y)
+        if self._desenho_atual:
+          self._desenho_atual.update( event.x, event.y)
           self.desenhar_fig()
         
     def incluir_figura_nova(self,event): 
-        if self.desenho_atual:
-            self.figuras.append((self.desenho_atual)) # salva a figura junto com a cor
+        if self._desenho_atual:
+            self._figuras.append((self._desenho_atual)) # salva a figura junto com a cor
+            self._desenho_atual= None
+            
         self.desenhar_fig()
     
     def desenhar_fig (self):
         #desenha na tela as figuras que estao prontas e guardadas
-        self.canvas.delete('all')#os desenhos estao guardados, o que apaga pra atualizar eh o que aparece na tela
+        self._canvas.delete('all')#os desenhos estao guardados, o que apaga pra atualizar eh o que aparece na tela
         
-        for desenho in self.figuras:#redesenha na tela os desenhos que estao guardados
-            desenho.desenhar(self.canvas)
+        for desenho in self._figuras:#redesenha na tela os desenhos que estao guardados
+            desenho.desenhar(self._canvas)
         
         #quando se desenha figuras novas(se desenho atual nao for nada)
         #if self.desenho_atual
-        if self.desenho_atual is not None:
-            self.desenho_atual.desenhar(self.canvas)
-            self.desenho_atual = None
-        self.desenhar_fig()
+        if self._desenho_atual is not None:
+            self._desenho_atual.desenhar(self._canvas)
+        #self.desenhar_fig()
         
     def clean_all (self):#limpa a tela
-        self.figuras.clear()
-        self.canvas.delete("all")
+        self._figuras.clear()
+        self._canvas.delete("all")
         print("Tela limpa")
