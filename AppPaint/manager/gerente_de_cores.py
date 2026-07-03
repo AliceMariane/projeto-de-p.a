4545b4e0369451dee99706591932d9bc47607ac8
Alice Mariane <ari60781326@gmail.com>
2026-07-02 21:23:11 -0300

    Refactor color management variable names

--- AppPaint/manager/gerente_de_cores.py
+++ AppPaint/manager/gerente_de_cores.py
@@ -3,24 +3,24 @@ class GerenteDeCores:
     Define as cores das figuras
     '''
     def __init__ (self):
-        self.cor_linha= 'black' #cor padrao, pra que seria rosa?
-        self.cor_de_preenchimento=''
+        self.cor_borda= 'black' #cor padrao, pra que seria rosa?
+        self.cor_preenchimento=''
         
         
-    def color_select_outline(self, novaCor): #cor que o usuario escolheu
-        self.cor_linha= novaCor
+    def color_select_outline(self, nova_cor): #cor que o usuario escolheu
+        self.cor_borda= nova_cor
 
-    def color_select_fill(self, novaCor): #cor que o usuario escolheu
-        self.cor_de_preenchimento= novaCor
+    def color_select_fill(self, nova_cor): #cor que o usuario escolheu
+        self.cor_preenchimento= nova_cor
         
     def wtfIsThisColor (self):
-        return self.cor_linha #a cor que vai usar nas figuras
+        return self.cor_borda #a cor que vai usar nas figuras
     
     def cor_atual_da_linha (self):
         return self.wtfIsThisColor()
 
     def cor_atual_do_preenchimento(self):
-        return self.cor_linha
+        return self.cor_preenchimento
 
 
 #talvez eu volte aqui
