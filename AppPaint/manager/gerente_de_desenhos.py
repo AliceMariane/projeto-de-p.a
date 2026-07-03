dd54f6d0fb996f5637beda2610779fa74458543a
Alice Mariane <ari60781326@gmail.com>
2026-07-02 21:10:50 -0300

    Refactor GerenteDeDesenhos class methods and attributes
    Refactor drawing manager to improve clarity and functionality.

--- AppPaint/manager/gerente_de_desenhos.py
+++ AppPaint/manager/gerente_de_desenhos.py
@@ -1,5 +1,5 @@
 #a fabrica tá na mesma pasta de gerente de desenhos
-from .fabrica_de_figuras import FabricaDeFiguras
+from fabrica_de_figuras import FabricaDeFiguras
 
 
 class GerenteDeDesenhos:
@@ -10,24 +10,45 @@ class GerenteDeDesenhos:
     def __init__(self, canvas, gerente_de_cores): #percebi que parte do que tem aqui tinha no main
         self.figuras= []#armazenar figuras
         self.canvas=canvas #os desenhos serao feitos aqui
-        self.desenhoatual= None
+        self.desenho_atual= None
         self.desenho= 'reta'#padrao, iria preferir que fosse rabisco, talvez troque pra rabisco depois
         self.gerente_de_cores= gerente_de_cores
         
-    def iniciar_figura(self, event):
-        ...
+    def iniciar_figura(self, event): #usa a fabrica pra montar o desenhos com os ingredientes 
+        self.desenho_atual= FabricaDeFiguras.criar(
+            self.desenho,
+            event.x,
+            event.y,
+            self.gerente_de_cores.cor_linha,
+            self.gerente_de_cores.cor_de_preenchimento
+            
+        )
         
     def update_fig(self,event):
-        ...
+        if self.desenho_atual:
+          self.desenho_atual.update( event.x, event.y)
+          self.desenhar_fig()
         
     def incluir_figura_nova(self,event):
-        ...
+        if self.desenho_atual:
+            self.figuras.append((self.desenho_atual)) # salva a figura junto com a cor
+        self.desenhar_fig()
     
     def desenhar_fig (self):
-        ...
+        #desenha as figuras que estao prontas
+        self.canvas.delete('all')#os desenhos estao guardados mas a tela apaga
         
-    def desenhar_new_fig (self):
-        ...
+        for desenho in self.figuras:#redesenha na tela os desenhos que estao guardados
+            desenho.desenhar(self.canvas)
         
-    def clean_all (self):
-        ...
+        #figuras novas(se desenho atual nao for nada)
+        #if self.desenho_atual
+        if self.desenho_atual is not None:
+            self.desenho_atual.desenhar(self.canvas)
+            self.desenho_atual = None
+        self.desenhar_fig()
+        
+    def clean_all (self):#limpa a tela
+        self.figuras.clear()
+        self.canvas.delete("all")
+        print("Tela limpa")
