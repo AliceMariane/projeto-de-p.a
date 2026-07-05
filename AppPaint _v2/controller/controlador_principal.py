e956d9c80514f70978cfad17e88a47f8cb558a3e
Alice Mariane <ari60781326@gmail.com>
2026-07-05 16:10:52 -0300

    Refactor ControladorPrincipal methods and add new ones

---
+++ AppPaint _v2/controller/controlador_principal.py
@@ -0,0 +1,18 @@
+class ControladorPrincipal:
+  def __init__(self):
+    pass
+
+  def iniciar_fig(self):
+    pass
+
+  def update_fig(self):
+    pass
+
+  def incluir_newfig (self):
+    pass
+
+  def set_fig (self):
+    pass
+    
+  def clean_all(self):
+    pass
