ece3a19611d3e47cb228ec51bfb3765588b1b56a
Alice Mariane <ari60781326@gmail.com>
2026-07-05 13:56:37 -0300

    Add main application entry point using Tkinter

---
+++ AppPaint _v2/__main__.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+from janela import Janela
+
+def main():
+    root = Tk()
+    app = Janela(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
