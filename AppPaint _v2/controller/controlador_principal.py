21fa504ed12455b58a4ec3c303b6f2f408058833
Alice Mariane <ari60781326@gmail.com>
2026-07-06 20:33:27 -0300

    commit do codigo do controlador principal
    Refactor ControladorPrincipal to include drawing functionality and integrate with model and view components.

--- AppPaint _v2/controller/controlador_principal.py
+++ AppPaint _v2/controller/controlador_principal.py
@@ -1,18 +1,53 @@
+from utils.cores import Cores
+from viewer.janela import Janela
+from controller.criar_figuras import CriarFiguras
+from dataclasses import dataclass, field
+from model.desenho import Desenho
+from model.figura import Figura
+
+#sera necessario criar a classe desenho no model para guardar e apagar os desenhos
+@dataclass
+
+
 class ControladorPrincipal:
-  def __init__(self):
-    pass
+  
+  #_figuras: list= field(default_factory=list) #armazenar figuras
+  _desenho_atual: Figura | None = None #default
+  _desenho:str = 'reta' #figura padrao
+  _cores: Cores= field(default_factory= Cores) #cores para as figuras
+  _janela : Janela= field(default_factory= Janela)
+  _model: Desenho = field(default_factory= Desenho)
+        
+  def iniciar_fig(self,x,y):
+    self._desenho_atual= CriarFiguras.criar(
+                                        self._desenho,
+                                        x,
+                                        y,
+                                        self._cores.cor_linha,
+                                        self._cores.cor_de_preenchimento
+            )
+    
+    
+    
 
-  def iniciar_fig(self):
-    pass
+  def update_fig(self, x, y):
+       if self._desenho_atual:
+        self._desenho_atual.update(x, y)
 
-  def update_fig(self):
-    pass
-
+        figuras = self._model.get_figuras + [self._desenho_atual]
+        self._janela.redesenhar(figuras)
+          
   def incluir_newfig (self):
-    pass
+    if self._desenho_atual:
+        self._model.adicionar(self._desenho_atual)
+        self._desenho_atual = None
 
-  def set_fig (self):
-    pass
+    self._janela.redesenhar(self._model.get_figuras())
+
+  def set_fig (self,desenho):
+    self._desenho=desenho
     
   def clean_all(self):
-    pass
+    self._model.clear()
+    self._desenho_atual = None
+    self._janela.redesenhar(self._model.figuras)
