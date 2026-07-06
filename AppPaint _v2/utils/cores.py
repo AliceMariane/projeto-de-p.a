9770d8e8381a189eb725b4765b80f8f0b6bf1523
Raí.Santos <70606883+F3ryG4rd@users.noreply.github.com>
2026-07-06 15:33:33 -0300

    Update cores.py

--- AppPaint _v2/utils/cores.py
+++ AppPaint _v2/utils/cores.py
@@ -2,44 +2,22 @@ class Cores:
     ''' 
     Define as cores das figuras
     '''
-    def __init__ (self):
+    
+    def __init__(self):
         self._cor_borda= 'black' #cor padrao
         self._cor_preenchimento='' #preenchimento inicial/padrao(vazio)
-        
+
     @property
-    def cor_atual_da_linha (self):#cor utilizada atualmente na borda/linha das figuras
+    def cor_borda(self): #cor utilizada atualmente na borda/linha das figuras
         return self._cor_borda
     @property
-    def cor_atual_do_preenchimento(self):#cor utilizada atualmente no preenchimento das figuras
+    def cor_preenchimento(self): #cor utilizada atualmente no preenchimento das figuras
         return self._cor_preenchimento
-
-    @cor_atual_da_linha.setter
-    def cor_atual_da_linha (self, nova_cor): #cor que o usuario escolheu para a borda/linha das figuras
-        self._cor_borda= nova_cor
-    @cor_atual_do_preenchimento.setter
-    def cor_atual_do_preenchimento(self, nova_cor): #cor que o usuario escolheu para o preenchimento das giguras
-        self._cor_preenchimento= nova_cor
-
-
-    #pra manter as alteracoes sem ter que voltar pra mexer em gerente de desenhos depois disso
-    #  |
-    #  |
-    #  v
-
-    
-    @property
-    def cor_borda(self):
-        return self._cor_borda
-    @property
-    def cor_preenchimento(self):
-        return self._cor_preenchimento
-
-
 
     @cor_borda.setter
-    def cor_borda(self, nova_cor):
+    def cor_borda(self, nova_cor): #cor que o usuario escolheu para a borda/linha das figuras
         self._cor_borda = nova_cor
 
     @cor_preenchimento.setter
-    def cor_preenchimento(self, nova_cor):
+    def cor_preenchimento(self, nova_cor): #cor que o usuario escolheu para o preenchimento das giguras
         self._cor_preenchimento = nova_cor
