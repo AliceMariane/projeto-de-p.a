10705ddb0efcd75fe7dccd0159530c3e5db3e127
Raí.Santos <70606883+F3ryG4rd@users.noreply.github.com>
2026-07-06 13:46:19 -0300

    Update figura.py

--- AppPaint _v2/model/figura.py
+++ AppPaint _v2/model/figura.py
@@ -1,3 +1,42 @@
-class Figura:
-  def__init__(self):
-    pass
+from abc import ABC, abstractmethod, dataclasses
+from dataclasses import dataclass, field
+
+@dataclass
+class Figura(ABC):
+    '''
+    Classe abstrata base para todas as figuras.
+    '''
+
+    _cor_borda : str = "black"
+    _cor_preenchimento : str = ""
+
+    @abstractmethod
+    def atualizar(self, x, y):
+        '''
+        Atualiza o ultimo ponto (x, y) da figura
+        Será implementado pelas subclasses
+        '''
+        pass
+
+    @abstractmethod
+    def desenhar(self, canvas):
+        '''
+        Desenha a figura
+        Será implementado pelas subclasses
+        '''
+        pass
+
+    @abstractmethod
+    def desenhar_preview(self, canvas):
+        '''
+        Preview da figura atual
+        '''
+        pass
+
+    @abstractmethod
+    def incompleta(self):
+        '''
+        Verifica se a figura pode ser salva
+        '''
+        pass
+    
