7dc8e24ed102ab883bdd6f7e9e9317c145ba0bf9
Alice Mariane <ari60781326@gmail.com>
2026-07-06 21:04:25 -0300

    pequenas correções e adição de comentários

--- AppPaint _v2/controller/controlador_principal.py
+++ AppPaint _v2/controller/controlador_principal.py
@@ -5,20 +5,19 @@ from dataclasses import dataclass, field
 from model.desenho import Desenho
 from model.figura import Figura
 
-#sera necessario criar a classe desenho no model para guardar e apagar os desenhos
+#a classe desenho armazena as figuras
 @dataclass
 
 
 class ControladorPrincipal:
   
-  #_figuras: list= field(default_factory=list) #armazenar figuras
   _desenho_atual: Figura | None = None #default
   _desenho:str = 'reta' #figura padrao
   _cores: Cores= field(default_factory= Cores) #cores para as figuras
   _janela : Janela= field(default_factory= Janela)
   _model: Desenho = field(default_factory= Desenho)
         
-  def iniciar_fig(self,x,y):
+  def iniciar_fig(self,x,y): #figura que esta sendo criado pelo usuario
     self._desenho_atual= CriarFiguras.criar(
                                         self._desenho,
                                         x,
@@ -27,27 +26,24 @@ class ControladorPrincipal:
                                         self._cores.cor_de_preenchimento
             )
     
-    
-    
-
-  def update_fig(self, x, y):
-       if self._desenho_atual:
+  def update_fig(self, x, y):#atualizar figura atual
+      if self._desenho_atual:
         self._desenho_atual.update(x, y)
 
-        figuras = self._model.get_figuras + [self._desenho_atual]
+        figuras = self._model.get_figuras()+ [self._desenho_atual]
         self._janela.redesenhar(figuras)
           
-  def incluir_newfig (self):
+  def incluir_newfig (self):#guardar e deixar na tela os desenhos que estao prontos
     if self._desenho_atual:
         self._model.adicionar(self._desenho_atual)
         self._desenho_atual = None
 
     self._janela.redesenhar(self._model.get_figuras())
 
-  def set_fig (self,desenho):
+  def set_fig (self,desenho):#alterar tipo de figura exemplo: 'reta'----->'circulo'
     self._desenho=desenho
     
-  def clean_all(self):
+  def clean_all(self):#manda apagar todos desenhos da tela
     self._model.clear()
     self._desenho_atual = None
-    self._janela.redesenhar(self._model.figuras)
+    self._janela.redesenhar(self._model.get_figuras())
