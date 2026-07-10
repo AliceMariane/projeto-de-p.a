8f16d42876c79a74b16fb55fea3aa330bd589334
Alice Mariane <ari60781326@gmail.com>
2026-07-09 21:39:24 -0300

    apenas um errinho ortografico
    Corrected a spelling error in the controller class name.

--- AppPaint _v2/__main__.py
+++ AppPaint _v2/__main__.py
@@ -4,7 +4,7 @@ from controller.controlador_principal import ControladorPrincipal
 if __name__ == "__main__":
     app = Janela()
 
-    controller = ControladorPricipal(_janela=app)
+    controller = ControladorPrincipal(_janela=app) # era algo bobo, apenas um erro ortografico que ocorreu
 
     app.notificar_controler = controller.notificar
 
