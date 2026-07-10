c174fbcaa21ebf9cee88a79d0e80c7689c759bee
caiquessouza7-oss <caiquessouza7@gmail.com>
2026-07-09 22:36:29 -0300

    Refactor Cores class using dataclass
    Refactor Cores class to use dataclass for cleaner syntax.

--- AppPaint _v2/utils/cores.py
+++ AppPaint _v2/utils/cores.py
@@ -1,23 +1,7 @@
-class Cores:   
-    ''' 
-    Define as cores das figuras
-    '''
+from dataclasses import dataclass
+
+@dataclass
+class Cores:
+    _cor_borda : str = 'black'
+    _cor_preenchimento : str = 'white'
     
-    def __init__(self):
-        self._cor_borda= 'black' #cor padrao
-        self._cor_preenchimento='' #preenchimento inicial/padrao(vazio)
-
-    @property
-    def cor_borda(self): #cor utilizada atualmente na borda/linha das figuras
-        return self._cor_borda
-    @property
-    def cor_preenchimento(self): #cor utilizada atualmente no preenchimento das figuras
-        return self._cor_preenchimento
-
-    @cor_borda.setter
-    def cor_borda(self, nova_cor): #cor que o usuario escolheu para a borda/linha das figuras
-        self._cor_borda = nova_cor
-
-    @cor_preenchimento.setter
-    def cor_preenchimento(self, nova_cor): #cor que o usuario escolheu para o preenchimento das giguras
-        self._cor_preenchimento = nova_cor
