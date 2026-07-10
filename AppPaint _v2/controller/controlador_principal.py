92b1a72837b8a891c81ee41fd5067d1786301625
Alice Mariane <ari60781326@gmail.com>
2026-07-09 21:37:10 -0300

    Refactor janela attribute to use object type

--- AppPaint _v2/controller/controlador_principal.py
+++ AppPaint _v2/controller/controlador_principal.py
@@ -14,7 +14,7 @@ class ControladorPrincipal:
   _desenho_atual: Figura | None = None #default
   _desenho:str = 'reta' #figura padrao
   _cores: Cores= field(default_factory= Cores) #cores para as figuras
-  _janela : Janela| None=None
+  _janela : object | None=None
   _model: Desenho = field(default_factory= Desenho)
         
   def iniciar_fig(self,x,y): #figura que esta sendo criado pelo usuario
@@ -25,13 +25,16 @@ class ControladorPrincipal:
                                         self._cores._cor_borda,
                                         self._cores._cor_preenchimento
             )
-    
+    print('iniciando figura')
   def update_fig(self, x, y):#atualizar figura atual
       if self._desenho_atual:
         self._desenho_atual.atualizar(x, y)
-
+       # print('atualizando figura')
         figuras = self._model.get_figuras()+ [self._desenho_atual]
+      #  print('chamando redesenhar')
         self._janela.redesenhar(figuras)
+        
+        
           
   def incluir_newfig (self):#guardar e deixar na tela os desenhos que estao prontos
     if self._desenho_atual:
@@ -46,18 +49,18 @@ class ControladorPrincipal:
   def clean_all(self):#manda apagar todos desenhos da tela
     self._model.clear()
     self._desenho_atual = None
-    self._janela.redesenhar(self._model.get_figuras())
+    self._janela.redesenhar(self._model.get_figuras(), self._desenho_atual)
 
 
   #aciona que funcao deve ser 'chamada' para cada acao
   def notificar(self, acao, valor):
-
+      
     acoes = {
         "selecionar_forma": lambda: self.set_fig(valor),
         "limpar_tela": self.clean_all,
-        "mudar_estilo": lambda: self.mudar_estilo(valor),
-        "selecionar_ferramenta": lambda: self.selecionar_ferramenta(valor),
-        "zoom": lambda: self.zoom(valor),
+        #"mudar_estilo": lambda: self.mudar_estilo(valor),
+        #"selecionar_ferramenta": lambda: self.selecionar_ferramenta(valor),
+        #"zoom": lambda: self.zoom(valor),
         "inicio": lambda: self.iniciar_fig(*valor),
         "arrastar": lambda: self.update_fig(*valor),
         "fim": self.incluir_newfig,
@@ -67,3 +70,4 @@ class ControladorPrincipal:
 
     if funcao:
         funcao()
+  #print('chamando')
