5204cd8beb87aab77f21762e6acb329adba683ab
Alice Mariane <ari60781326@gmail.com>
2026-07-11 22:13:40 -0300

    Add EstadoElipse class for ellipse user interaction
    Implement behavior for ellipse interaction with canvas.

---
+++ AppPaint _v2/controller/estado_elipse.py
@@ -0,0 +1,18 @@
+from .estado import Estado
+
+
+class EstadoElipse (Estado):
+    ''' 
+    define o comportamento da elipse durante
+    a interação do usuario com o canvas          
+    '''
+    def clicar (self, controlador_principal,event):
+        controlador_principal.iniciar_fig(event.x, event.y)
+    
+    
+    def arrastar (self, controlador_principal, event):
+        controlador_principal.update_fig(event.x, event.y)
+    
+    
+    def soltar (self, controlador_principal, event):
+        controlador_principal.incluir_newfig()
