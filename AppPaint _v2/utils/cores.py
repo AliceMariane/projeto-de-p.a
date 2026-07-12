f97b5a1a0ac97add553337bf109e602d83ce84c3
Raí Cerqueira <70606883+RaiSantos99@users.noreply.github.com>
2026-07-12 19:59:57 -0300

    Adição de @getters e @setters cores.py

--- AppPaint _v2/utils/cores.py
+++ AppPaint _v2/utils/cores.py
@@ -4,4 +4,35 @@ from dataclasses import dataclass
 class Cores:
     _cor_borda : str = 'black'
     _cor_preenchimento : str = ''
-    
+
+    @property
+    def cor_borda(self):
+        '''
+        Retorna a cor da borda.
+        '''
+
+        return self._cores.borda
+     
+    @cor_borda.setter
+    def cor_borda(self, cor):
+        '''
+        Atualiza a cor da borda.
+        '''
+
+        self._cores.borda = cor
+
+    @property
+    def cor_preenchimento(self):
+        '''
+        Retorna a cor do preenchimento.
+        '''
+
+        return self._cores.preenchimento
+
+    @cor_preenchimento.setter
+    def cor_preenchimento(self, cor):
+        '''
+        Atualiza a cor do preenchimento.
+        '''
+
+        self._cores.preenchimento = cor
