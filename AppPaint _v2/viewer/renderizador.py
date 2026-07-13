4a223a8942a5fa95f653140776e1a2799f05247b
Raí Cerqueira <70606883+RaiSantos99@users.noreply.github.com>
2026-07-12 21:25:59 -0300

    Correção em relação as cores renderizador.py

--- AppPaint _v2/viewer/renderizador.py
+++ AppPaint _v2/viewer/renderizador.py
@@ -1,26 +1,28 @@
 from abc import ABC, abstractmethod
 
+
 class Renderizador(ABC):
-    '''
-    Classe base para todos os renderizadores.
+  '''
+  Classe base para todos os renderizadores.
 
-    Cada subclasse sabe desenhar um tipo específico
-    de figura no Canvas.
+  Cada subclasse sabe desenhar um tipo específico
+  de figura no Canvas.
+  '''
+
+  @staticmethod
+  @abstractmethod
+  def desenhar(canvas, figura, cores):
+    '''
+    Desenha definitivamente uma figura.
     '''
 
-    @staticmethod
-    @abstractmethod
-    def desenhar(canvas, figura):
-        '''
-        Desenha a figura
-        Será implementado pelas subclasses
-        '''
-        pass  
-    
-    @staticmethod
-    @abstractmethod
-    def desenhar_preview(canvas, figura, dash= (4,2)):
-        '''
-        Preview da figura atual
-        '''
-        pass
+    pass
+
+  @staticmethod
+  @abstractmethod
+  def desenhar_preview(canvas, figura, cores, dash=(4, 2)):
+    '''
+    Desenha o preview da figura.
+    '''
+
+    pass
