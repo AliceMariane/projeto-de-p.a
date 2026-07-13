5cd9bfb0dd1040ade1ab810a13167101621a499f
Raí Cerqueira <70606883+RaiSantos99@users.noreply.github.com>
2026-07-12 21:33:30 -0300

    Atualizando em relação a classe Cores criar_figuras.py

--- AppPaint _v2/controller/criar_figuras.py
+++ AppPaint _v2/controller/criar_figuras.py
@@ -1,4 +1,4 @@
-#importando as classes de figuras
+# importando as classes de figuras
 from model.reta import Reta
 from model.circulo import Circulo
 from model.elipse import Elipse
@@ -7,30 +7,31 @@ from model.retangulo import Retangulo
 from model.quadrado import Quadrado
 
 
-class  CriarFiguras:
+class CriarFiguras:
+  '''
+  Cria os objetos das classes de figuras
+  de acordo com a escolha do usuário.
+  '''
+
+  @staticmethod
+  def criar(desenho, x, y):
     '''
-    cria os objetos das classes de figuras de acordo com a escolha do usuario
+    Cria uma nova figura.
     '''
+
+    # Estrutura dicionário para as formas
+
+    mapa_fig = {
+      'reta': Reta,
+      'retangulo': Retangulo,
+      'circulo': Circulo,
+      'elipse': Elipse,
+      'quadrado': Quadrado,
+      'maolivre': MaoLivre,
+    }
+
+    classe_fig = mapa_fig.get(desenho, MaoLivre)
+
+    nova_fig = classe_fig(x, y)
     
-    @staticmethod
-    def criar (desenho,x, y, cor_borda, cor_preenchimento):
-
-        #Estrura dicionario para as formas
-        
-        mapa_fig = {
-            'reta': Reta,
-            'retangulo': Retangulo,
-            'circulo': Circulo,
-            'elipse': Elipse,
-            'quadrado': Quadrado,
-            'maolivre': MaoLivre,
-        }
-
-        Classefig = mapa_fig.get(desenho, MaoLivre)
-
-        nova_fig = Classefig(x, y)
-
-        nova_fig._cor_borda = cor_borda
-        nova_fig._cor_preenchimento = cor_preenchimento
-
-        return nova_fig
+    return nova_fig
