bd28dce05f7d54811fed0d2b6878045cf35dcd63
Raí Cerqueira <70606883+RaiSantos99@users.noreply.github.com>
2026-07-12 21:23:58 -0300

    Remoção de cores, funções de desenhos e correções/adições em relação às @Property's reta.py

--- AppPaint _v2/model/reta.py
+++ AppPaint _v2/model/reta.py
@@ -1,81 +1,127 @@
 from dataclasses import dataclass, field
 from .figura import Figura
 
+
 @dataclass
 class Reta(Figura):
   '''
   Representa uma reta desenhada pelo usuário.
-  
+
   O primeiro click do mouse define o ponto inicial,
   o ponto final começa igual ao ponto inicial.
   '''
 
   # Primeiro Vértice (x, y)
-  _ini_x : int
-  _ini_y : int
-  
+  _ini_x: int
+  _ini_y: int
+
   # Segundo Vértice (x, y)
-  _fim_x : int = field(init= False)
-  _fim_y : int = field(init= False)
-  
+  _fim_x: int = field(init=False)
+  _fim_y: int = field(init=False)
+
+  def __post_init__(self):
+    '''
+    Inicializa o ponto final da reta
+    com as mesmas coordenadas do ponto inicial.
+    '''
+
+    self.fim_x = self.ini_x
+    self.fim_y = self.ini_y
+
+  @property
+  def ini_x(self):
+    '''
+    Retorna a coordenada x do ponto inicial.
+    '''
+
+    return self._ini_x
+
+  @ini_x.setter
+  def ini_x(self, valor):
+    '''
+    Atualiza a coordenada x do ponto inicial.
+    '''
+
+    self._ini_x = valor
+
+  @property
+  def ini_y(self):
+    '''
+    Retorna a coordenada y do ponto inicial.
+    '''
+
+    return self._ini_y
+
+  @ini_y.setter
+  def ini_y(self, valor):
+    '''
+    Atualiza a coordenada y do ponto inicial.
+    '''
+
+    self._ini_y = valor
+
+  @property
+  def fim_x(self):
+    '''
+    Retorna a coordenada x do ponto final.
+    '''
+
+    return self._fim_x
+
+  @fim_x.setter
+  def fim_x(self, valor):
+    '''
+    Atualiza a coordenada x do ponto final.
+    '''
+
+    self._fim_x = valor
+
+  @property
+  def fim_y(self):
+    '''
+    Retorna a coordenada y do ponto final.
+    '''
+
+    return self._fim_y
+
+  @fim_y.setter
+  def fim_y(self, valor):
+    '''
+    Atualiza a coordenada y do ponto final.
+    '''
+
+    self._fim_y = valor
+
   @property
   def pontos(self):
     '''
     Retorna os vértices da reta.
     '''
-    
+
     return (
-      self._ini_x,
-      self._ini_y,
-      self._fim_x,
-      self._fim_y
+      self.ini_x,
+      self.ini_y,
+      self.fim_x,
+      self.fim_y
     )
-    
-  def __post_init__(self):
-    '''
-    Inicializa o ponto final da reta
-    com as mesmas coordenadas do ponto inicial
-    '''
-    
-    self._fim_x = self._ini_x
-    self._fim_y = self._ini_y
 
   def atualizar(self, x, y):
     '''
-    Atualiza o ultimo ponto (x, y) da reta.
+    Atualiza o último ponto (x, y) da reta.
     '''
-    
-    self._fim_x = x
-    self._fim_y = y
 
-  def desenhar(self, canvas):
-    '''
-    Desenha de forma definitiva a reta na tela.
-    '''
-    
-    canvas.create_line(*self.pontos, 
-                        fill= self._cor_borda)
-
-  def desenhar_preview(self, canvas):
-    '''
-    Mostra o tracejado da reta em tempo
-    real enquanto o botão do mouse está sendo pressionado.
-    '''
-    
-    canvas.create_line(*self.pontos, 
-                      fill= self._cor_borda,
-                      dash= (4, 2))
+    self.fim_x = x
+    self.fim_y = y
 
   def incompleta(self):
     '''
-    Verifica se a Reta é válida.
-    
-    Uma reta é considerada incompleta 
+    Verifica se a reta é válida.
+
+    Uma reta é considerada incompleta
     quando os pontos são iguais.
     '''
-    
+
     return (
-    self._ini_x == self._fim_x and
-    self._ini_y == self._fim_y
+      self.ini_x == self.fim_x and
+      self.ini_y == self.fim_y
     )
-    
