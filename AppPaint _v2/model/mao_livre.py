80328c91b5ac79c143e4ee357de11ea0ad066dad
Raí Cerqueira <70606883+RaiSantos99@users.noreply.github.com>
2026-07-12 21:22:11 -0300

    Remoção de cores, funções de desenhos e adições/correções em relação às @Property's mao_livre.py

--- AppPaint _v2/model/mao_livre.py
+++ AppPaint _v2/model/mao_livre.py
@@ -1,65 +1,48 @@
 from dataclasses import dataclass, field
 from .figura import Figura
 
+
 @dataclass
 class MaoLivre(Figura):
   '''
-  Representa um desenho à mão livre.
+  Representa um rabisco desenhado pelo usuário.
 
-  Diferentemente das demais figuras,
-  o desenho livre é formado por uma sequência
-  de pontos atualizados em tempo real
-  durante o movimento do mouse.
-
-  Esses pontos são ligados
-  formando uma única linha.
+  A figura é composta por uma sequência
+  de pontos percorridos pelo mouse.
   '''
-  
-  _ini_x : int
-  _ini_y : int
-  
-  _pontos: list[tuple[int, int]] = field(init= False)
-  
-  def __post_init__(self):
-    self._pontos = [(self._ini_x, self._ini_y)]
+
+  _pontos: list = field(default_factory=list)
 
   @property
   def pontos(self):
     '''
     Retorna a lista de pontos do rabisco.
     '''
-    
+
     return self._pontos
-    
+
+  @pontos.setter
+  def pontos(self, valor):
+    '''
+    Atualiza a lista de pontos do rabisco.
+    '''
+
+    self._pontos = valor
+
   def atualizar(self, x, y):
     '''
-    Adiciona um novo ponto (x, y) ao rabisco
+    Adiciona um novo ponto ao rabisco.
     '''
-    
-    self._pontos.append((x, y))
 
-  def desenhar(self, canvas):
-    '''
-    Desenha de forma definitiva o rabisco na tela
-    '''
-    
-    canvas.create_line(self.pontos, 
-                        fill= self._cor_borda)
+    self.pontos.append((x,y))
 
-  def desenhar_preview(self, canvas):
-    '''
-    Mostra o tracejado do rabisco em tempo
-    real enquanto o botão do mouse está sendo pressionado
-    '''
-    
-    canvas.create_line(self.pontos, 
-                        fill= self._cor_borda,
-                        dash= (4, 2))
 
   def incompleta(self):
     '''
-    Verifica se existe pontos suficientes
-    para ser considerado uma figura válida
+    Verifica se o rabisco é válido.
+
+    Um rabisco é considerado incompleto
+    quando possui menos de dois pontos.
     '''
-    
-    return len(self._pontos) <= 1
+
+    return len(self.pontos) < 2
