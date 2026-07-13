31310d85cba579e4a6837bee5e6e0af85e9ffd2c
Raí Cerqueira <70606883+RaiSantos99@users.noreply.github.com>
2026-07-12 21:27:40 -0300

    Corrigindo funções de desenho renderizador_elipse.py

--- AppPaint _v2/viewer/renderizadores/renderizador_elipse.py
+++ AppPaint _v2/viewer/renderizadores/renderizador_elipse.py
@@ -1,38 +1,32 @@
 from viewer.renderizador import Renderizador
 
 
-class RenderizadorElipse(Renderizador):
+class RenderizadorMaoLivre(Renderizador):
+  '''
+  Classe responsável por desenhar
+  figuras à mão livre.
+  '''
+
+  @staticmethod
+  def desenhar(canvas, figura, cores):
     '''
-    Classe responsável por desenhar elipses.
+    Desenha o rabisco.
     '''
 
-    @staticmethod
-    def desenhar(canvas, elipse):
-        '''
-        Desenha uma elipse.
-        '''
+    canvas.create_line(
+      figura.pontos,
+      fill= cores.cor_borda,
+      smooth= True
+    )
 
-        canvas.create_oval(
-            elipse.ini_x,
-            elipse.ini_y,
-            elipse.fim_x,
-            elipse.fim_y,
-            outline=elipse.cor_borda,
-            fill=elipse.cor_preenchimento
-        )
+  @staticmethod
+  def desenhar_preview(canvas, figura, cores, dash=(4, 2)):
+    '''
+    Desenha o preview do rabisco.
+    '''
 
-    @staticmethod
-    def desenhar_preview(canvas, elipse, dash=(4, 2)):
-        '''
-        Desenha o preview da elipse.
-        '''
-
-        canvas.create_oval(
-            elipse.ini_x,
-            elipse.ini_y,
-            elipse.fim_x,
-            elipse.fim_y,
-            outline=elipse.cor_borda,
-            fill='',
-            dash=dash
-        )
+    canvas.create_line(
+      figura.pontos,
+      fill= cores.cor_borda,
+      smooth= True
+    )
