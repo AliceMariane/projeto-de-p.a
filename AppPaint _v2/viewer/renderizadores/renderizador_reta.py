18971c6eda3e2fe95035ab8030d934cb20df69a9
Raí Cerqueira <70606883+RaiSantos99@users.noreply.github.com>
2026-07-12 21:30:18 -0300

    Corrigindo funções de desenho renderizador_reta.py

--- AppPaint _v2/viewer/renderizadores/renderizador_reta.py
+++ AppPaint _v2/viewer/renderizadores/renderizador_reta.py
@@ -2,29 +2,29 @@ from viewer.renderizador import Renderizador
 
 
 class RenderizadorReta(Renderizador):
+  '''
+  Responsável por desenhar retas.
+  '''
+
+  @staticmethod
+  def desenhar(canvas, reta, cores):
     '''
-    Responsável por desenhar retas.
+    Desenha a reta.
     '''
 
-    @staticmethod
-    def desenhar(canvas, reta):
-        '''
-        Desenha a reta.
-        '''
+    canvas.create_line(
+      *reta.pontos,
+      fill= cores.cor_borda
+    )
 
-        canvas.create_line(
-            *reta.pontos,
-            fill= reta.cor_borda
-        )
+  @staticmethod
+  def desenhar_preview(canvas, reta, cores, dash=(4, 2)):
+    '''
+    Desenha o preview da reta.
+    '''
 
-    @staticmethod
-    def desenhar_preview(canvas, reta, dash=(4, 2)):
-        '''
-        Desenha o preview da reta.
-        '''
-
-        canvas.create_line(
-            *reta.pontos,
-            fill= reta.cor_borda,
-            dash= dash
-        )
+    canvas.create_line(
+      *reta.pontos,
+      fill= cores.cor_borda,
+      dash= dash
+    )
