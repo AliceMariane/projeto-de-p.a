17ab32eac1c337ffb5a268436c591d27c558f495
Raí Cerqueira <70606883+RaiSantos99@users.noreply.github.com>
2026-07-12 21:18:37 -0300

    Remoção de cores, funções de desenhos e correções em relação às @property's circulo.py

--- AppPaint _v2/model/circulo.py
+++ AppPaint _v2/model/circulo.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 from .figura import Figura
 
+
 @dataclass
 class Circulo(Figura):
   '''
@@ -10,101 +11,94 @@ class Circulo(Figura):
   o raio é recalculado em tempo real utilizando
   a distância entre o centro e a posição atual do mouse.
   '''
+
   _cx: int
   _cy: int
 
   _raio: float = field(init=False)
-  
+
   def __post_init__(self):
-    '''  
-    Inicializa o circulo com raio 0
     '''
-    
-    self._raio = 0
-  
+    Inicializa o círculo com raio 0.
+    '''
+
+    self.raio = 0
+
   @property
   def centro(self):
     '''
-    Retorna as coordenadas do centro
+    Retorna as coordenadas do centro.
     '''
-    
-    return (self.cx, self.cy)
-  
+
+    return (
+      self.cx,
+      self.cy
+    )
+
   @property
   def cx(self):
+    '''
+    Retorna a coordenada x do centro.
+    '''
+
     return self._cx
-  
+
   @property
   def cy(self):
+    '''
+    Retorna a coordenada y do centro.
+    '''
+
     return self._cy
-  
+
   @property
   def limites(self):
     '''
-    Retorna o quadrado delimitador do círculo
+    Retorna o quadrado delimitador do círculo.
     '''
-    
+
     return (
-            self.cx - self.raio,
-            self.cy - self.raio, 
-            self.cx + self.raio, 
-            self.cy + self.raio
-            )
-  
+      self.cx - self.raio,
+      self.cy - self.raio,
+      self.cx + self.raio,
+      self.cy + self.raio
+    )
+
   @property
   def raio(self):
     '''
-    Retorna o raio do circulo
+    Retorna o raio do círculo.
     '''
-    
+
     return self._raio
-  
+
   @raio.setter
   def raio(self, valor):
     '''
-    Atualiza o valor do raio
+    Atualiza o valor do raio.
     '''
-    
+
     self._raio = valor
-    
+
   def atualizar(self, x, y):
     '''
-    Atualiza o raio do Círculo
-    
+    Atualiza o raio do círculo.
+
     O raio é a distância do centro
-    até o ponto atual do mouse
+    até o ponto atual do mouse.
     '''
-    
-    self.raio = ((x - self.cx)**2 +
-                 (y- self.cy)**2 
-                ) **0.5
-  
-  def desenhar(self, canvas):
-    '''
-    Desenha de forma definitiva o cículo na tela
-    '''
-    
-    canvas.create_oval(*self.limites,
-                      outline= self._cor_borda, 
-                      fill= self._cor_preenchimento)
-  
-  def desenhar_preview(self, canvas):
-    '''
-    Mostra o tracejado do círculo em tempo
-    real enquanto o botão do mouse está sendo pressionado
-    '''
-    
-    canvas.create_oval(*self.limites,
-                      outline= self._cor_borda, 
-                      fill= self._cor_preenchimento,
-                      dash= (4, 2))
-  
+
+    self.raio = (
+      (x - self.cx) ** 2 +
+      (y - self.cy) ** 2
+    ) ** 0.5
+
   def incompleta(self):
     '''
     Verifica se o círculo pode ser criado.
-    
+
     O círculo é considerado inválido
-    se o raio for igual a 0
+    se o raio for igual a 0.
     '''
-    
+
     return self.raio == 0
