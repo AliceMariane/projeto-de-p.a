1698db864382df4f28226482c65805ebd3b1956d
Raí Cerqueira <70606883+RaiSantos99@users.noreply.github.com>
2026-07-12 21:29:03 -0300

    Corrigindo funções de desenho renderizador_maolivre.py

--- AppPaint _v2/viewer/renderizadores/renderizador_maolivre.py
+++ AppPaint _v2/viewer/renderizadores/renderizador_maolivre.py
@@ -1,36 +1,32 @@
 from viewer.renderizador import Renderizador
 
+
 class RenderizadorMaoLivre(Renderizador):
+  '''
+  Classe responsável por desenhar
+  figuras à mão livre.
+  '''
+
+  @staticmethod
+  def desenhar(canvas, figura, cores):
     '''
-    Classe responsável por desenhar
-    figuras à mão livre.
+    Desenha o rabisco.
     '''
 
-    @staticmethod
-    def desenhar(canvas, figura):
-        '''
-        Desenha o rabisco.
-        '''
+    canvas.create_line(
+      figura.pontos,
+      fill= cores.cor_borda,
+      smooth= True
+    )
 
-        # Desenha todos os pontos armazenados
-        # na figura utilizando uma linha suavizada.
-        canvas.create_line(
-            figura.pontos,
-            fill= figura.cor_borda,
-            smooth= True
-        )
+  @staticmethod
+  def desenhar_preview(canvas, figura, cores, dash=(4, 2)):
+    '''
+    Desenha o preview do rabisco.
+    '''
 
-    @staticmethod
-    def desenhar_preview(canvas, figura, dash= (4, 2)):
-        '''
-        Desenha o preview do rabisco.
-
-        Para desenhos à mão livre, o preview é
-        igual ao desenho definitivo
-        '''
-
-        canvas.create_line(
-            figura.pontos,
-            fill= figura.cor_borda,
-            smooth= True
-        )
+    canvas.create_line(
+      figura.pontos,
+      fill= cores.cor_borda,
+      smooth= True
+    )
