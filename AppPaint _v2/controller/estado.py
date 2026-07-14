7cc3a292fff0577031d76ef8710af396c2a1b70e
Alice Mariane <ari60781326@gmail.com>
2026-07-14 19:53:05 -0300

    Add new methods for figure manipulation

--- AppPaint _v2/controller/estado.py
+++ AppPaint _v2/controller/estado.py
@@ -15,7 +15,24 @@ class Estado(ABC):
   @abstractmethod
   def arrastar (self, controlador, x, y):
         pass
-    
+
+  
   @abstractmethod
   def soltar (self, controlador):
+        pass
+
+  
+  def copiar_figura(self):
+        pass
+
+  
+  def colar_figura(self):
+        pass
+  
+  
+  def trazer_para_frente(self):
+        pass
+  
+
+  def jogar_para_tras(self):
         pass
