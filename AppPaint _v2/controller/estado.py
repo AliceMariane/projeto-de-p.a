527c453825655abe35a83df94ba018bf9badbdb4
caiquessouza7-oss <caiquessouza7@gmail.com>
2026-07-14 01:13:38 -0300

    Refactor Estado class method signatures and comments

--- AppPaint _v2/controller/estado.py
+++ AppPaint _v2/controller/estado.py
@@ -4,20 +4,18 @@ from abc import ABC, abstractmethod
 
 class Estado(ABC):
 
-    ''' 
-    define o comportamento comum das ferramentas
-    quando o usuario interage com o canvas
-    '''
-    
-    @abstractmethod
-    def clicar (self, controlador_principal,event):
+  ''' define o comportamento comum das ferramentas
+      quando o usuario interage com o canvas
+  '''
+  @abstractmethod
+  def clicar (self, controlador, x, y):
         pass
     
     
-    @abstractmethod
-    def arrastar (self, controlador_principal, event):
+  @abstractmethod
+  def arrastar (self, controlador, x, y):
         pass
     
-    @abstractmethod
-    def soltar (self, controlador_principal, event):
+  @abstractmethod
+  def soltar (self, controlador):
         pass
