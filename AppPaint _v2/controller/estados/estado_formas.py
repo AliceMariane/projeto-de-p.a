4c485eaf5fe45b25e2c4404b5efcd678ce7cdaf7
caiquessouza7-oss <caiquessouza7@gmail.com>
2026-07-14 01:36:28 -0300

    Add estado_formas.py file

---
+++ AppPaint _v2/controller/estados/estado_formas.py
@@ -0,0 +1,13 @@
+from controller.estado import Estado
+
+class EstadoFormas(Estado):
+    
+    ''' serve para qualquer figura'''
+    def clicar(self, controlador, x, y):
+        controlador.iniciar_fig(x, y)
+
+    def arrastar(self, controlador, x, y):
+        controlador.update_fig(x, y)
+
+    def soltar(self, controlador):
+        controlador.inclui_newfig()
