c0cf0ca20948f72146dc7bdf0729e9678f124fae
Raí Cerqueira <70606883+RaiSantos99@users.noreply.github.com>
2026-07-15 18:32:10 -0300

    correção estado_selecionar.py

--- AppPaint _v2/controller/estados/estado_selecionar.py
+++ AppPaint _v2/controller/estados/estado_selecionar.py
@@ -1,13 +1,28 @@
 from controller.estado import Estado
 
-class EstadoFormas(Estado):
-    
-    ''' serve para qualquer figura'''
+
+class EstadoSelecionar(Estado):
+    '''
+    Estado responsável pela seleção e movimentação das figuras.
+
+    Este estado apenas encaminha os eventos do mouse para o
+    ControladorPrincipal, que possui toda a lógica da aplicação.
+    '''
+
     def clicar(self, controlador, x, y):
-        controlador.iniciar_fig(x, y)
+        '''
+        Solicita ao controlador que selecione uma figura.
+        '''
+        controlador.selecionar_figura(x, y)
 
     def arrastar(self, controlador, x, y):
-        controlador.update_fig(x, y)
+        '''
+        Solicita ao controlador mover a figura selecionada.
+        '''
+        controlador.mover_figura(x, y)
 
     def soltar(self, controlador):
-        controlador.incluir_newfig()
+        '''
+        Finaliza o movimento.
+        '''
+        controlador.finalizar_movimento()
