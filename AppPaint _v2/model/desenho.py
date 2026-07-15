74c4ce4ed69e3904de041d7122b5428d3e5522d2
Alice Mariane <ari60781326@gmail.com>
2026-07-15 11:20:58 -0300

    Add methods to manage figure layering

--- AppPaint _v2/model/desenho.py
+++ AppPaint _v2/model/desenho.py
@@ -14,3 +14,25 @@ class Desenho:
     def set_figuras(self, new_fig):
         self.figuras.clear()
         self.figuras.extend(new_fig)
+
+    def apagar (self, figura):
+        self.figuras.remove(figura)
+     
+     # apaga a  figura para depois inserir ela no fundo das outras
+    def jogar_para_tras(self, figura):
+        self.figuras.remove(figura)
+        self.figuras.insert(0, figura)
+    
+    # apaga a figura para depois inserir ela na frente das outras
+    def trazer_para_frente(self, figura):
+        self.figuras.remove(self, figura)
+        self.figuras.append(figura)
+        
+        
+    def buscar_figura(self, x, y):
+        for figura in reversed(self.figuras):
+            if figura.contem_ponto(x,y):
+                return figura
+        
+        return None
+    
