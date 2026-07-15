54453c1a435ecbeb122e06f4e8d0817b0917f26c
Raí Cerqueira <70606883+RaiSantos99@users.noreply.github.com>
2026-07-15 18:31:25 -0300

    refatoração de estado_selecionar.py

--- AppPaint _v2/controller/estados/estado_selecionar.py
+++ AppPaint _v2/controller/estados/estado_selecionar.py
@@ -1,56 +1,13 @@
 from controller.estado import Estado
-from dataclasses import dataclass
 
+class EstadoFormas(Estado):
+    
+    ''' serve para qualquer figura'''
+    def clicar(self, controlador, x, y):
+        controlador.iniciar_fig(x, y)
 
-@dataclass
-class EstadoSelecionar(Estado):
-    
-    """
-    define o comportamento de selecionar
-    as figuras (para, por exemplo:arrastar, copiar,         sobrepor)
-    
-    """
-    
-    #desenho: desenho = desenho
-    figura_selecionada = None
-    
-    last_x: int = 0
-    last_y: int = 0
-       
-    def clicar (self, x, y):
-        print('cliquei ', x, y)
-        self.figura_selecionada= self.desenho.buscar_figura(x,y)
-        print('selecionei ', self.figura_selecionada)
-        self.ultimo_x = x
-        self.ultimo_y = y   
-                       # buscar_figura é uma funcao que  
-                       # sera criada na classe
-                       # desenho que esta no model 
-                       # pra poder apagar, copiar, mudar
-                       # a figura clicada
-  
-    def arrastar (self, x, y):
-        if self.figura_selecionada: #se a figura estiver sendo arrastada 
-        
-            dx = x - self.last_x
-            dy = y - self.last_y
-            #dx, dy ----> quanto que a figura foi movida
-            #desde a ultima posicao dela
-            
-            self.figura_selecionada.mover(dx, dy)
-             
-            self.last_x = x
-            self.last_y = y
-       
-                        
-    def soltar (self, x, y):
-        pass
-      
-    def trazer_para_frente(self):
-        self.controlador.model.trazer_para_frente (self.figura_selecionada)
-        
-    def jogar_para_tras(self):
-        self.controlador.model.jogar_para_tras (self.figura_selecionada)
-        
-    def apagar (self):
-        self.controlador.model.apagar (self.figura_selecionada)
+    def arrastar(self, controlador, x, y):
+        controlador.update_fig(x, y)
+
+    def soltar(self, controlador):
+        controlador.incluir_newfig()
