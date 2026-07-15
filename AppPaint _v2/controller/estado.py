085fd1a5e78d7da2d0f670df0164dfae0589a7f6
Raí Cerqueira <70606883+RaiSantos99@users.noreply.github.com>
2026-07-15 18:27:32 -0300

    Correção nos métodos estado.py

--- AppPaint _v2/controller/estado.py
+++ AppPaint _v2/controller/estado.py
@@ -1,38 +1,21 @@
 from abc import ABC, abstractmethod
 
 
-
 class Estado(ABC):
 
-  ''' define o comportamento comum das ferramentas
+      '''
+      define o comportamento comum das ferramentas
       quando o usuario interage com o canvas
-  '''
-  @abstractmethod
-  def clicar (self, controlador, x, y):
-        pass
-    
-    
-  @abstractmethod
-  def arrastar (self, controlador, x, y):
-        pass
+      '''
+      
+      @abstractmethod
+      def clicar (self, controlador, x, y):
+            pass
+      
+      @abstractmethod
+      def arrastar (self, controlador, x, y):
+            pass
 
-  
-  @abstractmethod
-  def soltar (self, controlador):
-        pass
-
-  
-  def copiar_figura(self):
-        pass
-
-  
-  def colar_figura(self):
-        pass
-  
-  
-  def trazer_para_frente(self):
-        pass
-  
-
-  def jogar_para_tras(self):
-        pass
+      @abstractmethod
+      def soltar (self, controlador):
+            pass
